23217c9c9cbdf8c4c8cc52eec826d9463e8600af
Finn, Ryan <ryan.finn@mines.sdsmt.edu>
2022-10-06 23:54:59 -0600

    ...

--- PA2/sim.py
+++ PA2/sim.py
@@ -4,118 +4,60 @@ Created on Tue Dec 14 15:04:22 2021
 @author: jonathan
 """
 import matplotlib as mpl
-import matplotlib.animation as anim
 import matplotlib.patches as patches
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import solve_ivp
-
-plt.rcParams['text.usetex'] = False
 
 
 class SinglePendulumCart:
-    def __init__(self, m1, m2, I2, l, w1, h1, w2, l2):
-        self.m1 = m1
-        self.m2 = m2
-        self.I2 = I2
-        self.l = l
+    def __init__(self, m, w, h, M, W, H, g):
+        self.m = m
+        self.L = h
+        self.M = M
+        self.W = W
+        self.g = g
+        self.Pole = patches.Rectangle((-w / 2, 0), w, h)
+        self.Cart = patches.Rectangle((-W / 2, -H / 2), W, H)
 
-        self.p1 = patches.Rectangle((-w1 / 2, -h1 / 2), w1, h1)
-        self.p2 = patches.Rectangle((-w2 / 2, 0), w2, l2)
+    def deriv(self, _, X, u, __):
+        m = self.m
+        L = self.L
+        M = self.M
+        g = self.g
 
-    def deriv(self, _, y, u, __):
-        g = 9.81
-        xdot = y[1]
-        theta = y[2]
-        thetadot = y[3]
+        v = X[1]
+        theta = X[2]
+        omega = X[3]
+        I = m * L * L / 3
+
         try:
             u = u[0]
         except IndexError:
             u = u
 
-        A = np.array([[-(self.m1 + self.m2), self.m2 * self.l * np.cos(theta)],
-                      [self.m2 * self.l * np.cos(theta), -(self.m2 * self.l ** 2 + self.I2)]])
-        b = np.array([[self.m2 * self.l * np.sin(theta) * thetadot ** 2 - u],
-                      [-self.m2 * self.l * np.sin(theta) * g]])
+        A = np.array([
+            [-(m + M), m * L * np.cos(theta)],
+            [-np.cos(theta), L + I / m / L]
+        ])
 
-        xddot, thetaddot = np.linalg.solve(A, b)
+        b = np.array([
+            [m * L * np.sin(theta) * omega * omega - u],
+            [g * np.sin(theta)]
+        ])
 
-        return np.array([xdot, xddot[0], thetadot, thetaddot[0]])
+        a, omega_dot = np.linalg.solve(A, b)
+        a = a[0]
+        omega_dot = omega_dot[0]
+
+        return np.array([v, a, omega, omega_dot])
 
     def draw(self, ax, y):
         x = y[0]
         theta = y[2]
 
-        t1 = mpl.transforms.Affine2D().translate(x, 0) + ax.transData
-        t2 = mpl.transforms.Affine2D().rotate(
-            theta) + mpl.transforms.Affine2D().translate(x, 0) + ax.transData
-        self.p1.set_transform(t1)
-        self.p2.set_transform(t2)
-        return self.p1, self.p2
+        t1 = mpl.transforms.Affine2D().rotate(theta) + mpl.transforms.Affine2D().translate(x, 0) + ax.transData
+        t2 = mpl.transforms.Affine2D().translate(x, 0) + ax.transData
 
+        self.Pole.set_transform(t1)
+        self.Cart.set_transform(t2)
 
-def main():
-    m1 = 1
-    m2 = 1
-    L = 1
-    I2 = 1 / 12 * m2 * L ** 2
-    l = L / 2
-    w1 = 0.4
-    h1 = 0.2
-    w2 = 0.1
-    l2 = L
-    singlePendulumCart = SinglePendulumCart(m1, m2, I2, l, w1, h1, w2, l2)
-
-    T = np.linspace(0, 10, 1001)
-    timestep = T[1] - T[0]
-
-    x = 0
-    xdot = 0
-    theta = 0.1
-    thetadot = 0
-
-    state = np.array([x, xdot, theta, thetadot])
-    state_labels = [r'$x$', r'$\dot x$', r'$\theta$', r'$\dot \theta$']
-
-    us = []
-    states = []
-    for _ in T:
-        u = [0]
-        us.append(u)
-        res = solve_ivp(singlePendulumCart.deriv, [0, timestep], state, args=[u, 0])
-        state = res.y[:, -1]
-        states.append(state)
-
-    states = np.array(states).T
-    print("states shape", states.shape)
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_xlim(-5, 5)
-    ax.set_ylim(-5, 5)
-    plt.axis('equal')
-
-    def init_patches():
-        y = states[:, 0]
-        ps = singlePendulumCart.draw(ax, y)
-        for patch in ps:
-            ax.add_patch(patch)
-        return ps
-
-    def animate(it):
-        y = states[:, it]
-        return singlePendulumCart.draw(ax, y)
-
-    a = anim.FuncAnimation(
-        fig, animate, init_func=init_patches, interval=(T[1] - T[0]) * 1000, frames=states.shape[1], repeat=False,
-        blit=True)
-
-    plt.figure()
-    plt.xlabel('Time (s)')
-    plt.plot(T, us, label=r'$u$')
-    for i in range(len(state)):
-        plt.plot(T, states[i], label=state_labels[i])
-    plt.legend()
-
-    plt.show()
-    return a
+        return self.Pole, self.Cart
